a83a313959495afde05fed405cdc1cc25b7fc125
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2025-11-06 17:55:29 +0900

    エラーのカスタムフックの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
+import { useEffect } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import axios from 'axios'
+import type { CsrfToken } from './types'
 import Auth from './components/Auth'
 import Todo from './components/Todo'
-import { useEffect } from 'react'
-import axios from 'axios'
-import type { CsrfToken } from './type'
 
 function App() {
   useEffect(() => {
     axios.defaults.withCredentials = true
     const getCsrfToken = async () => {
       const { data } = await axios.get<CsrfToken>(
-        `${import.meta.env.VITE_API_URL}/csrf`
+        `${import.meta.env.REACT_APP_API_URL}/csrf`
       )
       axios.defaults.headers.common['X-CSRF-Token'] = data.csrf_token
     }
     getCsrfToken()
   }, [])
-
   return (
     <BrowserRouter>
       <Routes>
